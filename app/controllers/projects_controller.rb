e2690d66df8905b39524b35f5c62c466d2912b51
Kathryn Armitage <kat_armitage1@hotmail.com>
2020-03-03 14:55:43 +0000

    editing form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :find_project, only: [:show, :edit, :update, :destroy]
 
   def index
--- db/migrate/20200302144854_add_column_to_user.rb
+++ db/migrate/20200302144854_add_column_to_user.rb
@@ -2,5 +2,7 @@ class AddColumnToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
     add_column :users, :location, :string
+    add_column :users, :bio, :text
+    add_column :users, :linkedin_url, :string
   end
 end
