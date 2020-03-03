1e2cf7a7fe5e8c1d909c100b59054d463bc5faa1
Tom Harwood <tharwood26@gmail.com>
2020-03-03 15:42:51 +0000

    couple of seeds added, linked to index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_125742) do
+ActiveRecord::Schema.define(version: 2020_03_03_140638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_125742) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.bigint "charity_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
