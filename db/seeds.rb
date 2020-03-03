1b393973457310d7b30f97a431e899feb64229f1
Tom Harwood <tharwood26@gmail.com>
2020-03-03 12:03:35 +0000

    added cloudinary for photos and files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,3 +14,7 @@
 # user = User.create!(email: 'test@test.com', password: 'test1234')
 
 # Project.create!
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+project = Project.new(title: 'NES', body: "A great console")
+project.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
