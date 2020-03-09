bcbbadf5f13282580b35e8c388abb28482484c8d
Kathryn Armitage <kat_armitage1@hotmail.com>
2020-03-09 19:44:50 +0000

    styled profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_165714) do
+ActiveRecord::Schema.define(version: 2020_03_09_111722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_165714) do
     t.string "linkedin_url"
     t.string "trello_token"
     t.boolean "coder"
+    t.string "github_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
